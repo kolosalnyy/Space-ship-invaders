e6ddbaa63c1664cb72de81e18db5b5eed19d88fb
kolosalnyy <mikomucha12@gmail.com>
2025-03-02 16:59:06 +0100

    dodalem troche z punktow i czcionke

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-print("Hello World!")
-print("Hello World ale od Denisa!")
-print("AHA GitJub")
+import pygame
+import random
+import math
+from pygame import mixer
 
-#chat-gpt pomoze?
-while Trure:
-    print("Smacznego")
+pygame.init()
+
+screen_width = 1000
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Welcome to Space Inviders Game by: GemiGames Studio")
+
+score_val = 0
+scoreX = 5
+scoreY = 5
+game_over_font = pygame.font.Font("freesansbold.ttf", 20)
+
+dalej mi sie nie chcialo uczyc lol
