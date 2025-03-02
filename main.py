fdda9a0c9956f3b74c61ae8baff580ed980e6854
GemanoutStudio <gemanoutstudiopl@gmail.com>
2025-03-02 17:51:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import pygame
+import pygame as pg
 import random
 import math
 from pygame import mixer
 
-pygame.init()
+pg.init()
 
-screen_width = 1000
-screen_height = 800
-screen = pygame.display.set_mode((screen_width, screen_height))
+#-------Zmienne--------#
+SCR_WG = 1000
+SCR_HG = 800
+screen = pg.display.set_mode((SCR_WG, SCR_HG))
+#----------------------#
 
-pygame.display.set_caption("Welcome to Space Inviders Game by: GemiGames Studio")
+pg.display.set_caption("Welcome to Space Inviders Game by: GemanoutGames Studio")
 
 score_val = 0
 scoreX = 5
 scoreY = 5
-game_over_font = pygame.font.Font("freesansbold.ttf", 20)
+game_over_font = pg.font.Font("freesansbold.ttf", 20)
 
-dalej mi sie nie chcialo uczyc lol
+#-----Końcówka nauki------#
