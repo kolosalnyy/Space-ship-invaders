4ed2aa561608da108f0a8b588e38cf80b78ea606
GemanoutStudio <gemanoutstudiopl@gmail.com>
2025-03-03 18:05:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ pg.init()
 SCR_WG = 1000
 SCR_HG = 800
 screen = pg.display.set_mode((SCR_WG, SCR_HG))
-#----------------------#
-
-pg.display.set_caption("Welcome to Space Inviders Game by: GemanoutGames Studio")
 
 score_val = 0
 scoreX = 5
 scoreY = 5
 game_over_font = pg.font.Font("freesansbold.ttf", 20)
+#----------------------#
+
+#-------Okno-----------#
+pg.display.set_caption("Welcome to Space Inviders Game by: GemanoutGames Studio")
+#----------------------#
 
 #-----Końcówka nauki------#
